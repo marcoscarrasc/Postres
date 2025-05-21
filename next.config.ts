5b119917cdd8959b10dc4a805983311712df8c2f
Marcos Carrasco <161668037+marcoscarrasc@users.noreply.github.com>
2025-05-21 11:41:08 -0500

    Configura despliegue en GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export',
+  basePath: '/Rostres', 
   images: {
     domains: ['highcook.com.pe'],
   },
-  output: 'export',
-  basePath: '/Rostres', 
+ 
 };
 
 export default nextConfig;
