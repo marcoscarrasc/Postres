661bf900fd44cf48823db730ab36aa9c4099cf60
Marcos Carrasco <161668037+marcoscarrasc@users.noreply.github.com>
2025-05-21 16:05:03 -0500

    desplegar

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export',
-  basePath: '/Rostres', 
+  
+ 
   images: {
     domains: ['highcook.com.pe'],
   },
