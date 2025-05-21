51e8260e859f0928855b2a454f9af455a0d7acac
Marcos Carrasco <161668037+marcoscarrasc@users.noreply.github.com>
2025-05-21 16:25:55 -0500

    .

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['highcook.com.pe'],
+  },
+  // Otras configuraciones si las necesitas
+}
+
+module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,15 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Postres Paolita",
-  description: "Postres al por mayor y menor",
-};
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
